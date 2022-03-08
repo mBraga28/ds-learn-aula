9be64965b43df24f2e5983ad1857814c3f60b75c
Marco Braga <maboa17@gmail.com>
2022-03-08 12:32:55 -0300

    Revert "Custom content for current user"
    This reverts commit d24f974fec5e37710e9fafe2b30a79aaa340ace8.

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/OfferRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/OfferRepository.java
@@ -2,7 +2,6 @@ package com.devsuperior.dslearnbds.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dslearnbds.entities.Course;
 import com.devsuperior.dslearnbds.entities.Offer;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
